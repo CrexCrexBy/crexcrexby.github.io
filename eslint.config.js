ec89d5bfcca61b1e3028300efb2d95904d3bd659
HjET <11130582+HjETWD@users.noreply.github.com>
2025-08-05 23:52:17 +0300

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js"
+import globals from "globals"
+import pluginReact from "eslint-plugin-react"
+import css from "@eslint/css"
+import { defineConfig } from "eslint/config"
+import eslintPluginPrettier from "eslint-plugin-prettier"
+import eslintConfigPrettier from "eslint-config-prettier"
+
+export default defineConfig([
+	{
+		files: ["**/*.{js,mjs,cjs,jsx}"],
+		plugins: {
+			js,
+			prettier: eslintPluginPrettier,
+		},
+		extends: ["js/recommended", eslintConfigPrettier],
+		rules: {
+			...eslintPluginPrettier.configs.recommended.pules,
+			"no-console": "warn",
+			eqeqeq: "warn",
+			curly: "warn",
+			"no-else-return": "warn",
+			indent: ["error", "tab"],
+		},
+		languageOptions: { globals: globals.browser },
+	},
+	pluginReact.configs.flat.recommended,
+	{
+		files: ["**/*.css"],
+		plugins: { css },
+		language: "css/css",
+		extends: ["css/recommended"],
+	},
+])
